b6bc7e622b1f18ee59263fa3f26bf9cdb4d53766
Simion <simion.okola@student.moringaschool.com>
2022-10-04 22:29:10 +0300

    ft--json database

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React from "react-dom";
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Farmers Assistant Tool</h2>
+    </div>
+  );
+}
+
+export default App;
