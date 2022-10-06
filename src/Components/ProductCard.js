f374999e0c23e021f1f13a4275197db2b477e23f
Simion <simion.okola@student.moringaschool.com>
2022-10-06 16:43:10 +0300

    ft-display card

---
+++ src/Components/ProductCard.js
@@ -0,0 +1,18 @@
+function ProductCard({expense}) {
+    function formatAmount(amount) {
+        const amountFormatter = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
+        return amountFormatter.format(amount);
+    }
+
+    return (
+        <div>
+             <h4>{expense.product}</h4>
+            <h4>{expense.category}</h4>
+            <p>{expense.date}</p>
+            <h2>{formatAmount(expense.amount)}</h2>
+            <p style={{'fontStyle': 'italic'}}>{expense.note}</p>
+        </div>
+    )
+}
+
+export default ProductCard;
