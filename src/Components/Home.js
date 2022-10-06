df0a7c006cd3646d0289ff20e6ae9636fa014e42
Simion <simion.okola@student.moringaschool.com>
2022-10-06 16:44:58 +0300

    ft-home

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import ProductCard from './ProductCard';
+
+function Home({expenses}) {
+    const expenseListUi = expenses.map( expense => <li key={expense.id}><ProductCard key={expense.id} expense={expense}/></li>)
+    return (
+        <div>
+            <p>Information on Crop Farming:</p>
+            <ul>{expenseListUi}</ul>
+        </div>
+    )
+}
+
+export default Home;
